a06135438228a86a470c500f7e45090d731a4f56
Nikolay Veselinov <nikolay@nikolay.veselinov>
2020-05-19 17:33:27 +0300

    Create writing migration

--- contrib/form_wizard/admin.py
+++ contrib/form_wizard/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Client
+from .models import Client, WritingClient
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'eng', 'phone', 'email')
+
+
+@admin.register(WritingClient)
+class WritingClientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'eng', 'phone', 'email')
--- contrib/form_wizard/models.py
+++ contrib/form_wizard/models.py
@@ -7,3 +7,11 @@ class Client(models.Model):
     eng = models.CharField('ЕГН', max_length=10)
     phone = models.CharField('Мобилен телефон', max_length=10)
     email = models.EmailField('E-mail',max_length=150)
+
+
+class WritingClient(models.Model):
+    first_name = models.CharField('Име', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    eng = models.CharField('ЕГН', max_length=10)
+    phone = models.CharField('Мобилен телефон', max_length=10)
+    email = models.EmailField('E-mail',max_length=150)
