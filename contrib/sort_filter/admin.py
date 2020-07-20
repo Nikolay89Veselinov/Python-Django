bba4ea0f26fa30fd159a1d27b986694de8c5217f
Nikolay Veselinov <nikolay@nikolay.veselinov>
2020-07-20 10:16:32 +0300

    Create list diplay method and short description

--- contrib/sort_filter/admin.py
+++ contrib/sort_filter/admin.py
@@ -14,4 +14,16 @@ class CityAdmin(admin.ModelAdmin):
 
 @admin.register(Pub)
 class PubAdmin(admin.ModelAdmin):
-    list_display = ('name', 'city', 'active')
+    list_display = ('name', 'city', 'active', 'get_city', 'get_country')
+
+    def get_city(self, obj):
+            return obj.city
+
+    def get_country(self, obj):
+            return obj.city.country.name
+
+    get_city.short_description = 'get city'
+    get_country.short_description = 'get country'
+
+
+    
