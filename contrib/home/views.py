5cdf4106b91f38269684423d30655c74ef34ffd1
Nikolay Veselinov <nikolay@nikolay.veselinov>
2020-08-19 11:19:12 +0300

    Add session views home page

--- contrib/home/views.py
+++ contrib/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def home(request):
-
-    return render(request, 'home.html')
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    if num_visits >= 5:
+        del(request.session['num_visits'])
+    context = {
+        'view_count': ('view count ' + str(num_visits))
+    } 
+    return render(request, 'home.html', context)
