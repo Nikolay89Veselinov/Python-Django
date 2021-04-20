0399dcd1569549621e17fe8f8b5ea5f097b60fe2
Nikolay Veselinov <nikolay@nikolay.veselinov>
2021-04-20 12:31:36 +0300

    fix settings

--- template_project/settings.py
+++ template_project/settings.py
@@ -99,7 +99,7 @@ LOGIN_URL = '/loginuser/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 # STATIC_ROOT = '/tmp/staticfiles'
