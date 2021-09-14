719c6bd3435bf999faf5e403cb9b519dadf80d18
Nikolay Veselinov <nikolay@nikolay.veselinov>
2021-09-14 16:21:43 +0300

    Revert "Test 3 git revert"
    This reverts commit 2e9ed2af8b38b2931cdc1913b72abbc09f43fd13.

--- contrib/home/views.py
+++ contrib/home/views.py
@@ -19,7 +19,6 @@ def home(request):
     num_visits = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_visits
     print('test git revert 2')
-    print('test git revert 3')
     if num_visits >= 5:
         del(request.session['num_visits'])
     context = {
