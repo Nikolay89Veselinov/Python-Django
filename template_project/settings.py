edda97264cfcec9ef3f689418cb06c66bf0384af
Nikolay Veselinov <Nikolay89Veselinov@gmail.com>
2022-05-19 18:34:31 +0300

    Change settings file

--- template_project/settings.py
+++ template_project/settings.py
@@ -111,15 +111,15 @@ LOGIN_URL = '/loginuser/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-# STATIC_ROOT = '/tmp/staticfiles'
-STATIC_ROOT = os.path.join(DATA_DIR, 'staticfiles')
+STATIC_ROOT = '/tmp/staticfiles'
+# STATIC_ROOT = os.path.join(DATA_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'template_project', 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'template_project', 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
